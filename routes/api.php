17d47c6199f274e6be6dc393d7fe86d760c934c7
Armand Iskandarsyah <armand.iskandarsyah@wingscorp.com>
2025-02-18 11:59:17 +0700

    fix naming

--- routes/api.php
+++ routes/api.php
@@ -4,11 +4,11 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Admin\CategoryController;
 use App\Http\Controllers\Admin\ProductController;
-use App\Http\Controllers\Admin\ProductImagesController;
+use App\Http\Controllers\Admin\ProductImageController;
 use App\Http\Controllers\Admin\ProductLinkController;
 use App\Http\Controllers\Admin\UserController;
 use App\Http\Controllers\Admin\VariantController;
-use App\Http\Controllers\Admin\VariantImagesController;
+use App\Http\Controllers\Admin\VariantImageController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
@@ -19,8 +19,8 @@ Route::prefix('admin')->group(function () {
     Route::resource('user', UserController::class);
     Route::resource('category', CategoryController::class);
     Route::resource('product', ProductController::class);
-    Route::resource('product_images', ProductImagesController::class);
+    Route::resource('product_images', ProductImageController::class);
     Route::resource('product_link', ProductLinkController::class);
     Route::resource('variant', VariantController::class);
-    Route::resource('variant_images', VariantImagesController::class);
+    Route::resource('variant_images', VariantImageController::class);
 });
