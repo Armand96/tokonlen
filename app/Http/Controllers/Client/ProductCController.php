85e91616ee0e7dbffa14c1392579f824c587c145
Armand Iskandarsyah <armand.iskandar96@gmail.com>
2025-02-23 21:45:20 +0700

    feat: add variant

--- app/Http/Controllers/Client/ProductCController.php
+++ app/Http/Controllers/Client/ProductCController.php
@@ -26,7 +26,7 @@ class ProductCController extends Controller
             $query->where('category_id', '=', $req->category_id);
         }
 
-        $query->where('is_active', true)->with(['image', 'discount'])->orderBy('id', 'desc');
+        $query->where('is_active', true)->with(['image', 'discount', 'variant.images'])->orderBy('id', 'desc');
         $products = $query->paginate($data_per_page);
         // dd(DB::getQueryLog());
         return $products;
