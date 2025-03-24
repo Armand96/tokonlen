f359adb38e2d8f4939a9b5193455db567876d21f
Armand Iskandarsyah <armand.iskandar96@gmail.com>
2025-03-24 22:53:04 +0700

    fix user seeder

--- database/seeders/Userseeder.php
+++ database/seeders/Userseeder.php
@@ -14,7 +14,7 @@ class Userseeder extends Seeder
      */
     public function run(): void
     {
-        $password = Hash::make('Tester2025!');
+        $password = Hash::make(env('DB_PASSWORD', 'admin'));
         $dbname = env('DB_DATABASE', 'tokonlen');
         DB::insert("INSERT INTO `$dbname`.`users` (`id`, `name`, `username`, `email`, `image`, `image_thumb`, `email_verified_at`, `password`, `remember_token`, `created_at`, `updated_at`) VALUES (1, 'Admin', 'admin', 'zhindayacatalog@gmail.com', NULL, NULL, NULL, '$password', NULL, NULL, NULL)");
     }
